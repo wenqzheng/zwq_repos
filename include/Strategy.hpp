04dead22c3b357a1b7d533310b4d56d0b31635c3
wenqzheng <2246074112@qq.com>
2018-02-05 21:51:12 +0800

    zwq commit

---
+++ include/Strategy.hpp
@@ -0,0 +1,47 @@
+#include <memory>
+#include <string>
+
+class Strategy
+{
+struct StrategyConcept
+{
+    virtual ~StrategyConcept() {}
+    virtual void do_something() const = 0;
+    virtual std::string return_something() const = 0;
+};
+
+template<typename S>
+struct StrategyModel:StrategyConcept
+{
+    StrategyModel(const S& s):strategy(s) {}
+    virtual void do_something() const
+    {
+        strategy.do_something();
+    }
+    virtual std::string return_something() const
+    {
+        return typeid(S).name();
+    }
+private:
+    S strategy;
+};
+
+std::shared_ptr<StrategyConcept> strategy;
+
+public:
+template<typename S>
+Strategy(const S& s)
+    :strategy(std::make_shared<StrategyModel<S>>(s))
+{}
+
+std::string return_something() const
+{
+    return strategy->return_something();
+}
+
+void do_something() const
+{
+    strategy->do_something();
+}
+
+};
--- queue.cpp
+++ queue.cpp
@@ -1,4 +1,5 @@
 #include "queue/blockingconcurrentqueue.h"
+#include "include/Strategy.hpp"
 #include <iostream>
 #include <thread>
 #include <memory>
@@ -7,7 +8,44 @@
 #include <typeinfo>
 using namespace std;
 
-using valueType = std::unique_ptr<std::string>;// std::shared_ptr<>
+class Stra1
+{
+public:
+    void do_something() const
+    {
+        cout << "num 1 Strategy" << endl;
+    }
+};
+
+class Stra2
+{
+public:
+    void do_something() const
+    {
+        cout << "num 2 Strategy" << endl;
+    }
+};
+
+class Stra3
+{
+public:
+    void do_something() const
+    {
+        cout << "num 3 Strategy" << endl;
+    }
+};
+
+template<int I>
+class Stra
+{
+public:
+    void do_something() const
+    {
+        cout << "num " << I << " Strategy" << endl;
+    }
+};
+
+using valueType =  std::shared_ptr<Strategy>;
 
 int main()
 {
@@ -17,9 +55,22 @@ int main()
     cout << atomic_is_lock_free(&Str) << endl;
     moodycamel::BlockingConcurrentQueue<valueType> q;
     std::thread producer([&]() {
-        string str;
-        cin >> str;
-        valueType pi = make_unique<string>(str);
+        int i;
+	cin >> i;
+	Stra1 s1;
+	Stra2 s2;
+	Stra3 s3;
+	valueType pi;
+	if (i == 1)
+            pi = make_shared<Strategy>(s1);
+	else if (i == 2)
+	    pi = make_shared<Strategy>(s2);
+	else if (i == 3)
+     	    pi = make_shared<Strategy>(s3);
+	else {
+	    cout << "error num" << endl;
+	    return;
+	}
         q.enqueue(std::move(pi));
         cout << std::this_thread::get_id() << endl;
     });
@@ -27,8 +78,10 @@ int main()
     std::thread consumer([&]() {
         valueType item;
         q.wait_dequeue(item);
-        cout << typeid(decltype(item)).name() << endl;
-        cout << *item << endl;
+        //cout << typeid(decltype(item)).name() << endl;
+	item->do_something();
+	cout << item->return_something() << endl;
+        //cout << *item << endl;
         cout << std::this_thread::get_id() << endl;
     });
 
