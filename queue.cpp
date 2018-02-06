4d0fa804a8a992ff4e60a8d04875be800f3ac2c8
wenqzheng <2246074112@qq.com>
2018-02-06 17:56:20 +0800

    zwq commit

--- queue.cpp
+++ queue.cpp
@@ -7,23 +7,25 @@
 #include <typeinfo>
 using namespace std;
 
+using valueType = std::unique_ptr<std::string>;// std::shared_ptr<>
+
 int main()
 {
     cout << std::thread::hardware_concurrency() << endl;
     cout << std::this_thread::get_id() << endl;
     std::shared_ptr<std::string> Str;
     cout << atomic_is_lock_free(&Str) << endl;
-    moodycamel::BlockingConcurrentQueue<std::shared_ptr<string>> q;
+    moodycamel::BlockingConcurrentQueue<valueType> q;
     std::thread producer([&]() {
         string str;
         cin >> str;
-        std::shared_ptr<string> pi = make_shared<string>(str);
-        q.enqueue(pi);
+        valueType pi = make_unique<string>(str);
+        q.enqueue(std::move(pi));
         cout << std::this_thread::get_id() << endl;
     });
 
     std::thread consumer([&]() {
-        std::shared_ptr<string> item;
+        valueType item;
         q.wait_dequeue(item);
         cout << typeid(decltype(item)).name() << endl;
         cout << *item << endl;
