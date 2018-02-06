385ac342286cb104878c9f80c5163b338ee5c26b
wenqzheng <2246074112@qq.com>
2018-02-06 16:58:45 +0800

    zwq commit

--- queue.cpp
+++ queue.cpp
@@ -1,12 +1,16 @@
 #include "queue/blockingconcurrentqueue.h"
 #include <iostream>
 #include <thread>
+#include <memory>
+#include <atomic>
 using namespace std;
 
 int main()
 {
     cout << std::thread::hardware_concurrency() << endl;
     cout << std::this_thread::get_id() << endl;
+    std::shared_ptr<std::string> Str;
+    cout << atomic_is_lock_free(&Str) << endl;
     moodycamel::BlockingConcurrentQueue<int> q;
     std::thread producer([&]() {
         int i = 0;
